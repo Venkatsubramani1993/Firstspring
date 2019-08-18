18f91deb02c6a376ca0661314be0178c0869930c
Venkatesan Subramani <venkatesan.subramani@verse.in>
2019-08-18 17:23:47 +0530

    FirstSpringProject

---
+++ first-springboot-project/src/main/java/com/in28minutes/springboot/WelcomeController.java
@@ -0,0 +1,27 @@
+package com.in28minutes.springboot;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+	@Autowired
+	private WelcomeService service;
+				
+@RequestMapping("/welcome")
+	public String welcome()
+	{
+		return service.retrieveWelcomeMessage();
+	}
+}
+
+@Component
+class WelcomeService{
+	public String retrieveWelcomeMessage(){
+		//Complex Method
+		return "Hai good Morning!";
+		
+	}
+}
